8d8e60a92ba4b366c5e81531844d49e37f46e184
cst204 <sutter1488@saskpolytech.ca>
2016-09-30 13:51:10 -0600

    wrote code for A_OpenAddressing.cs and created the KeyValue class

--- HashTables/HashTables/A_OpenAddressing.cs
+++ HashTables/HashTables/A_OpenAddressing.cs
@@ -11,5 +11,76 @@ namespace HashTables
         where V : IComparable<V>
     {
 
+        // used to get the increment value (varies based on the implementation
+        protected abstract int GetIncrement(int iAttempt, K key);
+
+        //Local instance of the prime number class
+        private PrimeNumber pn = new PrimeNumber();
+
+        //constructor to set up the hashtable array
+        public A_OpenAddressing()
+        {
+            oDataArray = new object[pn.getNextPrime()];
+        }
+
+        public override void Add(K key, V vValue)
+        {
+            //How many attempts were made to increment
+            int iAttempt = 1;
+            //get the initial hash of the key
+            int iInitialHash = HashFunction(key);
+            // the current location 
+            int iCurrentLocation = iInitialHash;
+            //position where we add
+            int iPositionToAdd = -1;
+
+            //Loop until we encounter a null (end of collision chain)
+            while(oDataArray[iCurrentLocation] != null)
+            {
+
+            }
+
+
+        }
+
+        public override void Remove(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override V Get(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerator<V> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < oDataArray.Length; i++)
+            {
+                sb.Append("Bucket " + i + ": ");
+                if (oDataArray[i] != null)
+                {
+                    if (oDataArray[i].GetType() == typeof(Tombstone))
+                    {
+                        sb.Append("Tombstone");
+                    }
+                    else
+                    {
+                        KeyValue<K, V> kv = (KeyValue<K, V>)oDataArray[i];
+                        sb.Append(kv.Value.ToString());
+                    }
+
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
     }
 }
---
+++ HashTables/HashTables/keyValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class keyValue<K,V>
+        where K : IComparable<K>
+        where V : IComparable<V>
+    {
+        //Store the key
+        K kKey;
+        //Store the value
+        V vValue;
+
+        public keyValue (K key, V value)
+        {
+            kKey = key;
+            vValue = value;
+        }
+
+        public K Key
+        {
+            get
+            {
+                return kKey;
+            }
+        }
+
+        public V Value
+        {
+            get
+            {
+                return vValue;
+            }
+        }
+    }
+}
