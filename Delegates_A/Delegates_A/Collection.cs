90111fefdb2d273fa1900fe7387a896d5ad7e335
cst204 <sutter1488@saskpolytech.ca>
2016-09-30 09:03:47 -0600

    initial project version

---
+++ Delegates_A/Delegates_A/Collection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_A
+{
+    //create a delegate data type
+    public delegate void HandleAnInt(int x);
+
+    //Define an enumeration that can be used as a direction flag
+    public enum Direction
+    {
+        FORWARD,
+        BACKWARD
+    }
+
+    public class Collection
+    {
+        private int[] iArray = { 5, 10, 20 };
+
+        public void iterate(HandleAnInt fp, Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.FORWARD:
+                    {
+                        for (int i = 0; i < iArray.Length; i++)
+                        {
+                            //Hard code a method that does something to the data
+                            //Console.WriteLine(iArray[i]);
+                            fp(iArray[i]);
+                        }
+                    }
+                    break;
+                case Direction.BACKWARD:
+                    {
+                        for (int i = iArray.Length - 1; i >= 0; i--)
+                        {
+                            fp(iArray[i]);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            
+        }
+    }
+}
---
+++ Delegates_A/Delegates_A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_A
+{
+    class Program
+    {
+        public static void DisplayInt(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public static void DisplaySquare(int a)
+        {
+            Console.WriteLine(a * a);
+        }
+
+        static void Main(string[] args)
+        {
+            Collection c = new Collection();
+            c.iterate(DisplaySquare, Direction.BACKWARD);
+        }
+    }
+}
