1296b881fd95b06a4f580bd41884dc25a43b4125
cst204 <sutter1488@saskpolytech.ca>
2016-09-30 14:52:59 -0600

    Second hour of class, finnished add method, created linear.cs, and tested. Some error need to be fixed.

---
+++ HashTables/HashTables/Linear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class Linear<K,V>: A_OpenAddressing<K,V>
+        where K : IComparable<K>
+        where V : IComparable<V>
+    {
+        protected override int GetIncrement(int iAttempt, K key)
+        {
+            int iIncrement = 1;
+            return iIncrement + iAttempt;
+        }
+    }
+}
