98d1b5ad3402a633d63a1130f491d82f495619ae
cst204 <sutter1488@saskpolytech.ca>
2016-12-06 09:05:59 -0600

    More sorting shit

---
+++ Sorting/Sorting/QuickSorterMedianOfThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class QuickSorterMedianOfThree<T> : QuickSorter<T> where T : IComparable<T>
+    {
+        public QuickSorterMedianOfThree(T[] array) : base(array)
+        {
+        }
+
+        //override the method that finds the pivot
+        protected override int FindPivot(int first, int last)
+        {
+            int mid = (first + last) / 2;
+            if (array[first].CompareTo(array[mid]) > 0)
+            {
+                Swap(first, mid);
+            }
+            if (array[first].CompareTo(array[last]) > 0)
+            {
+                Swap(first, last);
+            }
+            if (array[mid].CompareTo(array[last]) > 0)
+            {
+                Swap(mid, last);
+            }
+            return mid;
+        }
+    }
+}
